8266ac9d38f2303d6de6f09edccfefd58f8cfc09
drakeerv <drakeerv@outlook.com>
2025-01-13 02:06:31 -0500

    Add initialization

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,16 @@ use godot::classes::{ISprite2D, Sprite2D};
 struct FastTerrain;
 
 #[gdextension]
-unsafe impl ExtensionLibrary for FastTerrain {}
+unsafe impl ExtensionLibrary for FastTerrain {
+    fn on_level_init(level: InitLevel) {
+        match level {
+            InitLevel::Editor => {
+                godot_print!("FastTerrain initialized!");
+            }
+            _ => {}
+        }
+    }
+}
 
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
@@ -12,14 +21,12 @@ struct Player {
     speed: f64,
     angular_speed: f64,
 
-    base: Base<Sprite2D>
+    base: Base<Sprite2D>,
 }
 
 #[godot_api]
 impl ISprite2D for Player {
     fn init(base: Base<Sprite2D>) -> Self {
-        godot_print!("Hello, world!"); // Prints to the Godot console
-        
         Self {
             speed: 400.0,
             angular_speed: std::f64::consts::PI,
@@ -28,12 +35,13 @@ impl ISprite2D for Player {
     }
 
     fn physics_process(&mut self, delta: f64) {
-        // In GDScript, this would be: 
+        // In GDScript, this would be:
         // rotation += angular_speed * delta
-        
+
         let radians = (self.angular_speed * delta) as f32;
         self.base_mut().rotate(radians);
-        // The 'rotate' method requires a f32, 
+        // The 'rotate' method requires a f32,
         // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
     }
 }
+
